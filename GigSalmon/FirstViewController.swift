a9516bb84411f4b3e43957d7e3b08add3254f1a7
Ryoichi Tanaka <ryoichitanakajp@gmail.com>
2015-04-07 13:55:00 +0900

    Initial Commit

---
+++ GigSalmon/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  GigSalmon
+//
+//  Created by tnk on 2015/04/07.
+//  Copyright (c) 2015年 tnk. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
