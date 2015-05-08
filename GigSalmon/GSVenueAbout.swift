839a9a47f4d9008e78d44915ce40ffc9489d3a3b
Ryoichi Tanaka <ryoichitanakajp@gmail.com>
2015-05-08 13:17:16 +0900

    Venue Aboutの画像表示処理を追加

--- GigSalmon/GSVenueAbout.swift
+++ GigSalmon/GSVenueAbout.swift
@@ -10,19 +10,67 @@ import UIKit
 import Parse
 
 class GSVenueAbout: UIViewController {
-
 	var venue: PFObject?
-	@IBOutlet weak var descriptionLabel: UILabel!
+	@IBOutlet weak var imageView0: UIImageView!
+	@IBOutlet weak var imageView1: UIImageView!
+	@IBOutlet weak var imageView2: UIImageView!
+	@IBOutlet weak var descriptionView: UITextView!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
 		if venue != nil {
-			descriptionLabel.text = venue!["description"] as? String
+			descriptionView.text = venue!["description"] as? String
+			self.loadImages()
 		}
 	}
 
+	override func viewWillAppear(animated: Bool) {
+		super.viewWillAppear(animated)
+		
+		
+	}
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
+	}
+	
+	func loadImages() {
+		var imageFile = self.venue!["aboutImage0"] as? PFFile
+		if imageFile != nil {
+			imageFile!.getDataInBackgroundWithBlock { (imageData: NSData?, error:
+				NSError?) -> Void in
+				if error == nil {
+					if let imageData = imageData {
+						let image = UIImage(data: imageData)
+						self.imageView0.image = image
+					}
+				}
+			}
+		}
+		imageFile = self.venue!["aboutImage1"] as? PFFile
+		if imageFile != nil {
+			imageFile!.getDataInBackgroundWithBlock { (imageData: NSData?, error:
+				NSError?) -> Void in
+				if error == nil {
+					if let imageData = imageData {
+						let image = UIImage(data: imageData)
+						self.imageView1.image = image
+					}
+				}
+			}
+		}
+		imageFile = self.venue!["aboutImage2"] as? PFFile
+		if imageFile != nil {
+			imageFile!.getDataInBackgroundWithBlock { (imageData: NSData?, error:
+				NSError?) -> Void in
+				if error == nil {
+					if let imageData = imageData {
+						let image = UIImage(data: imageData)
+						self.imageView2.image = image
+					}
+				}
+			}
+		}
 	}
 }
